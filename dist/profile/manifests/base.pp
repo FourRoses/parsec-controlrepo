d0a49f459f3fdcc63297785f67403924ce914410
Juanjo Presa <Juanjo Presa>
2015-08-27 15:43:29 +0200

    Adapted apt to new module version

--- dist/profile/manifests/base.pp
+++ dist/profile/manifests/base.pp
@@ -17,38 +17,38 @@ class profile::base {
   if $::osfamily == 'Debian' {
     class {'::apt':
       purge => {
-        sources.list       => true,
-        sources.list.d     => true,
-        preferences.list   => true,
-        preferences.list.d => true,
+        'sources.list'       => true,
+        'sources.list.d'     => true,
+        'preferences.list'   => true,
+        'preferences.list.d' => true,
       }
     }
 
     apt::source {'ubuntu':
-      location    => 'http://16.0.96.20:3142/archive.ubuntu.com/ubuntu',
-      repos       => 'main restricted universe multiverse',
+      location => 'http://16.0.96.20:3142/archive.ubuntu.com/ubuntu',
+      repos    => 'main restricted universe multiverse',
     }
     apt::source {'ubuntu-security':
-      location    => 'http://16.0.96.20:3142/archive.ubuntu.com/ubuntu',
-      repos       => 'main restricted universe multiverse',
-      release     => "${lsbdistcodename}-security",
+      location => 'http://16.0.96.20:3142/archive.ubuntu.com/ubuntu',
+      repos    => 'main restricted universe multiverse',
+      release  => "${lsbdistcodename}-security",
     }
     apt::source {'ubuntu-updates':
-      location    => 'http://16.0.96.20:3142/archive.ubuntu.com/ubuntu',
-      repos       => 'main restricted universe multiverse',
-      release     => "${lsbdistcodename}-updates",
+      location => 'http://16.0.96.20:3142/archive.ubuntu.com/ubuntu',
+      repos    => 'main restricted universe multiverse',
+      release  => "${lsbdistcodename}-updates",
     }
     apt::source {'ubuntu-backports':
-      location    => 'http://16.0.96.20:3142/archive.ubuntu.com/ubuntu',
-      repos       => 'main restricted universe multiverse',
-      release     => "${lsbdistcodename}-backports",
+      location => 'http://16.0.96.20:3142/archive.ubuntu.com/ubuntu',
+      repos    => 'main restricted universe multiverse',
+      release  => "${lsbdistcodename}-backports",
     }
     apt::source {'puppetlabs':
-      location    => 'http://16.0.96.20/repo/apt.puppetlabs.com',
-      repos       => 'main dependencies',
-      key         => {
-        id     => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
-        source => 'http://16.0.96.20/repo/puppet/pubkey.gpg',
+      location => 'http://16.0.96.20/repo/apt.puppetlabs.com',
+      repos    => 'main dependencies',
+      key      => {
+        'id'     => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
+        'source' => 'http://16.0.96.20/repo/puppet/pubkey.gpg',
       }
     }
   }
