95830925d32e9076921d2a7214f287e7a4971e56
Juanjo Presa <Juanjo Presa>
2015-08-27 14:27:19 +0200

    Added udev rules to profile

--- dist/profile/manifests/sivestacion.pp
+++ dist/profile/manifests/sivestacion.pp
@@ -15,6 +15,6 @@ class profile::sivestacion {
   }
   udev::rule { '70-persistent-net.rules':
     ensure  => present,
-    content => "SUBSYSTEM=='net', ACTION=='add', DRIVERS=='?*', ATTR{address}=='${::foreman_interfaces[0][mac]}', ATTR{dev_id}=='0x0', ATTR{type}=='1', KERNEL=='eth*', NAME='${::foreman_interfaces[0][identifier]}'\n",
+    content => "SUBSYSTEM==\"net\", ACTION==\"add\", DRIVERS==\"?*\", ATTR{address}==\"${::foreman_interfaces[0][mac]}\", ATTR{dev_id}==\"0x0\", ATTR{type}==\"1\", KERNEL==\"eth*\", NAME=\"${::foreman_interfaces[0][identifier]}\"\n",
   }
 }
