be0b8c15ff6cc3b52241f1b9e13b28cd73567e51
Juanjo Presa <Juanjo Presa>
2015-08-27 15:41:50 +0200

    Adapted apt to new module version

--- dist/profile/manifests/base.pp
+++ dist/profile/manifests/base.pp
@@ -16,40 +16,40 @@ class profile::base {
 
   if $::osfamily == 'Debian' {
     class {'::apt':
-      purge_sources_list   => true,
-      purge_sources_list_d => true,
-      purge_preferences_d  => true,
+      purge => {
+        sources.list       => true,
+        sources.list.d     => true,
+        preferences.list   => true,
+        preferences.list.d => true,
+      }
     }
 
     apt::source {'ubuntu':
       location    => 'http://16.0.96.20:3142/archive.ubuntu.com/ubuntu',
       repos       => 'main restricted universe multiverse',
-      include_src => false,
     }
     apt::source {'ubuntu-security':
       location    => 'http://16.0.96.20:3142/archive.ubuntu.com/ubuntu',
       repos       => 'main restricted universe multiverse',
       release     => "${lsbdistcodename}-security",
-      include_src => false,
     }
     apt::source {'ubuntu-updates':
       location    => 'http://16.0.96.20:3142/archive.ubuntu.com/ubuntu',
       repos       => 'main restricted universe multiverse',
       release     => "${lsbdistcodename}-updates",
-      include_src => false,
     }
     apt::source {'ubuntu-backports':
       location    => 'http://16.0.96.20:3142/archive.ubuntu.com/ubuntu',
       repos       => 'main restricted universe multiverse',
       release     => "${lsbdistcodename}-backports",
-      include_src => false,
     }
     apt::source {'puppetlabs':
       location    => 'http://16.0.96.20/repo/apt.puppetlabs.com',
       repos       => 'main dependencies',
-      include_src => false,
-      key         => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
-      key_source  => 'http://16.0.96.20/repo/puppet/pubkey.gpg',
+      key         => {
+        id     => '47B320EB4C7C375AA9DAE1A01054B7A24BD6EC30',
+        source => 'http://16.0.96.20/repo/puppet/pubkey.gpg',
+      }
     }
   }
 
