80e28bb3f5f829fb4a893624e035876fe80b63c0
Juanjo Presa <Juanjo Presa>
2015-08-27 10:31:44 +0200

    fixed typo

--- dist/profile/manifests/base.pp
+++ dist/profile/manifests/base.pp
@@ -25,12 +25,12 @@ class profile::base {
     apt::source {'ubuntu-security':
       location => 'http://16.0.96.20:3142/archive.ubuntu.com/ubuntu',
       repos    => 'main restricted universe multiverse',
-      releases => '${lsbdistcodename}-security',
+      releases => "${lsbdistcodename}-security",
     }
     apt::source {'ubuntu-updates':
       location => 'http://16.0.96.20:3142/archive.ubuntu.com/ubuntu',
       repos    => 'main restricted universe multiverse',
-      releases => '${lsbdistcodename}-updates',
+      releases => "${lsbdistcodename}-updates",
     }
     apt::source {'ubuntu-backports':
       location => 'http://16.0.96.20:3142/archive.ubuntu.com/ubuntu',
