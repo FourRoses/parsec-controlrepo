1d07fcb507d1baebb7ec144c1f3f50536b8c2d50
Juanjo Presa <Juanjo Presa>
2015-08-27 10:17:21 +0200

    init network class with foreman data

--- dist/profile/manifests/sivestacion.pp
+++ dist/profile/manifests/sivestacion.pp
@@ -1,5 +1,4 @@
 class profile::sivestacion {
-  class {'::networkdeb': } ->
   class {'::csh': } ->
   class {'::user::siv': } ->
   class {'::dkms': } ->
@@ -9,5 +8,15 @@ class profile::sivestacion {
 
   package {['libncurses5-dev','libssl-dev','libssl0.9.8']:
     ensure => installed,
+  }
+
+  network_config { $::foreman_interfaces[0][identifier]:
+    ensure    => 'present',
+    family    => 'inet',
+    ipaddress => $::foreman_interfaces[0][ip],
+    method    => 'static',
+    mode      => 'raw',
+    netmask   => '255.255.255.0',
+    onboot    => 'true',
   }
 }
