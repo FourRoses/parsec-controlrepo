e815844badd33a5e2c91e64f073d757955910ebb
Juanjo Presa <Juanjo Presa>
2015-08-27 13:59:55 +0200

    Added gateway to network

--- dist/profile/manifests/sivestacion.pp
+++ dist/profile/manifests/sivestacion.pp
@@ -8,18 +8,9 @@ class profile::sivestacion {
   package {['libncurses5-dev','libssl-dev','libssl0.9.8']: }
   package {'tcsh': }
 
-#  network_config { $::foreman_interfaces[0][identifier]:
-#    ensure    => 'present',
-#    family    => 'inet',
-#    ipaddress => $::foreman_interfaces[0][ip],
-#    method    => 'static',
-#    mode      => 'raw',
-#    netmask   => '255.255.255.0',
-#    onboot    => 'true',
-#  }
-
   network::interface { $::foreman_interfaces[0][identifier]:
     ipaddress => $::foreman_interfaces[0][ip],
     netmask   => $::foreman_interfaces[0][attrs][netmask],
+    gateway   => regsubst($::foreman_interfaces[0][ip], '^(\d+\.\d+\.\d+\.)\d+$', '\1.1')
   }
 }
