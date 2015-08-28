397c90ba8eec8b3d268a1474f82f027704669188
Juanjo Presa <Juanjo Presa>
2015-08-28 14:07:14 +0200

    vmtools no manage dev packages

--- dist/profile/manifests/vmtools.pp
+++ dist/profile/manifests/vmtools.pp
@@ -19,10 +19,11 @@ class profile::vmtools {
 
   if $vmtools_mode == 'vmwaretools' {
     class { 'vmwaretools':
-      version     => '9.0.10-1481436',
-      working_dir => '/tmp/vmwaretools',
-      archive_url => 'http://16.0.96.20/repo/vmwaretools',
-      archive_md5 => '0a11d80baf58a1bd4df2e2a8c5b32a24',
+      version         => '9.0.10-1481436',
+      working_dir     => '/tmp/vmwaretools',
+      archive_url     => 'http://16.0.96.20/repo/vmwaretools',
+      archive_md5     => '0a11d80baf58a1bd4df2e2a8c5b32a24',
+      manage_dev_pkgs => false,
     }
   }
   if $vmtools_mode == 'openvmtools' {
