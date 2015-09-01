1771ba09637c0f1c305f6c4b01128bd220cf4342
Juanjo Presa <Juanjo Presa>
2015-09-01 14:58:00 +0200

    Refactor POI profile

---
+++ dist/profile/manifests/nvidia.pp
@@ -0,0 +1,3 @@
+class profile::x11::nvidia inherits profile::x11 {
+  include ::nvidia
+}
--- dist/profile/manifests/poi.pp
+++ dist/profile/manifests/poi.pp
@@ -3,17 +3,91 @@ class profile::poi (
 ) {
 
   class {'::snmp':
-    agentaddress      => hiera('snmp::agentaddress'),
-    views             => hiera('snmp::views'),
-    com2sec           => hiera('snmp::com2sec'),
+    agentaddress => hiera('snmp::agentaddress'),
+    views        => hiera('snmp::views'),
+    com2sec      => hiera('snmp::com2sec'),
   }
 
   include ::interfaces
-  include ::metro
-  include ::xprintidle
+
+  # ::metro
+  user { 'opermm':
+    ensure     => 'present',
+    uid        => '502',
+    gid        => '501',
+    groups     => 'uucp',
+    comment    => 'Usuario que lanza el CTC',
+    home       => '/home/opermm',
+    managehome => true,
+    shell      => '/bin/bash',
+    password   => '$1$2ytnzzj8$jQpaUPnccZDw2YVpzqzNs/',
+  }
+  group { 'ctcing':
+    ensure => 'present',
+    gid    => '501',
+  }
+
+  # ::xprintidle
+  package {'libXScrnSaver':
+    ensure  => latest,
+  }
+  file {'/usr/bin/xprintidle':
+    ensure  => present,
+    replace => false,
+    source  => 'puppet:///data/poi/xprintidle.i386',
+    mode    => '0755',
+  }
+
   include ::incron
-  include ::swaks
-  include ::zenity
+
+  # ::swaks
+  package {'swaks':
+    ensure => installed,
+  }
+
+  # ::zenity
+  package {'zenity' :
+    ensure => installed,
+  }
+
+  # ::xdotool
+  file {'/var/tmp/xdotool.rpm':
+    ensure => file,
+    source => 'puppet:///data/poi/xdotool-2.20110530.1-1.of.el5.i386.rpm',
+  }
+  package {'xdotool':
+    ensure   => present,
+    provider => 'rpm',
+    source   => '/var/tmp/xdotool.rpm',
+  }
+
+  # ::audio
+  package {'alsa-utils': }
+  package {'sox': }
+  exec {'/bin/alsaunmute 0':
+    onlyif  => '/usr/bin/amixer get -c 0 Master | grep off]',
+    require => Package['alsa-utils'],
+  }
+
+  # ::firefox
+  package {'firefox':
+    ensure => installed,
+  }
+
+  # ::java
+  file {'/var/tmp/jre-6u29-linux-i586.rpm':
+    ensure => present,
+    source => 'puppet:///data/poi/jre-6u29-linux-i586.rpm',
+  } ->
+  package {'jre-1.6.0_29-fcs':
+    ensure   => installed,
+    provider => 'rpm',
+    source   => '/var/tmp/jre-6u29-linux-i586.rpm',
+  } ->
+  class {'java':
+    package => 'jre-1.6.0_29-fcs',
+  }
+
   include ::metro::misc::captura
   include ::metro::misc::fonts
   include ::metro::misc::bashprofile
@@ -22,53 +96,94 @@ class profile::poi (
     include metro::misc::compartido
   }
   if member($apps, 'ctc') {
-    include profile::ctc
-    include profile::ats
+    include metro::xsession
+    include metro::xresources
+    include metro::misc::xlinks
+    include metro::ctc
+    include metro::misc::botones
+    include metro::misc::rkiosk
+    class {'metro::tn':
+      autostart  => false,
+      monitor    => false,
+      decoration => 'border',
+    }
+    class {'metro::sirei':
+      autostart  => false,
+      monitor    => false,
+      decoration => 'border',
+    }
   }
   if member($apps, 'rctc') {
-    include profile::rctc
+    include metro::xsession_replica
+    include metro::xresources
+    include metro::misc::xlinks
+    class { 'metro::ctc':
+      xsession => '.xsession_replica',
+    }
   }
   if member($apps, 'igcamaras') {
-    include profile::igcamaras
+    include metro::xsession
+    include metro::igcamaras
   }
   if member($apps, 'megafonia') {
-    include profile::megafonia
+    include metro::xsession
+    include metro::xresources
+    include metro::megafonia
   }
   if member($apps, 'siv') {
-    include profile::siv
+    include metro::xsession
+    include metro::xresources
+    include metro::misc::xlinks
+    include metro::sico::keys
+    class {'metro::sico': } ->
+    class {'metro::siv::client': }
   }
   if member($apps, 'xpostit') {
-    include profile::xpostit
+    include metro::xsession
+    include metro::xpostit
   }
   if member($apps, 'enrutador') {
-    include profile::enrutador
+    include metro::xsession
+    include metro::enrutador
   }
   if member($apps, 'tn') {
-    include profile::tn
+    include metro::xsession
+    include metro::tn
   }
   if member($apps, 'sirei') {
-    include profile::sirei
+    include metro::xsession
+    include metro::sirei
   }
   if member($apps, 'rt') {
-    include profile::rt
+    include metro::xsession
+    include metro::xresources
+    include metro::rt
   }
   if member($apps, 'ctcligero') {
-    include profile::ctcligero
+    include metro::xsession
+    include metro::ctcligero
   }
   if member($apps, 'tce') {
-    include profile::tce
+    include metro::xsession
+    include metro::xresources
+    include metro::misc::xlinks
+    include metro::sico::keys
+    class {'metro::sico': } ->
+    class {'metro::tce::client' : }
   }
   if member($apps, 'isacd') {
-    include profile::isacd
+    include metro::xsession
+    class {'metro::isacd':}
   }
-#  if member($apps, 'ats') {
-#    include profile::ats
-#  }
   if member($apps, 'vncpogui') {
-    include profile::vncpogui
+    include ::vncviewer
+    include metro::vncpogui
+    class {'elotouch':
+      monitor => hiera(metro::vncpogui::monitor)
+    }
   }
   if member($apps, 'herramienta') {
-    include profile::herramienta
+    include metro::herramienta
   }
   if member($apps, 'controlpantallas') {
     include metro::misc::controlpantallas
