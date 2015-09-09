dc95d066813ba50166c71c76ee1cb765c15a64e4
Juanjo Presa <Juanjo Presa>
2015-09-09 11:11:41 +0200

    second network interface as optional

--- dist/profile/manifests/poi.pp
+++ dist/profile/manifests/poi.pp
@@ -26,10 +26,13 @@ class profile::poi (
     hwaddr    => $::foreman_interfaces[0][mac],
     gateway   => regsubst($::foreman_interfaces[0][ip], '^(\d+\.\d+\.\d+)\.\d+$', '\1.1')
   }
-  network::interface { $::foreman_interfaces[1][identifier]:
-    ipaddress => $::foreman_interfaces[1][ip],
-    netmask   => $::foreman_interfaces[1][subnet][mask],
-    hwaddr    => $::foreman_interfaces[1][mac],
+
+  if $::foreman_interfaces[1] {
+    network::interface { $::foreman_interfaces[1][identifier]:
+      ipaddress => $::foreman_interfaces[1][ip],
+      netmask   => $::foreman_interfaces[1][subnet][mask],
+      hwaddr    => $::foreman_interfaces[1][mac],
+    }
   }
 
   # ::metro
