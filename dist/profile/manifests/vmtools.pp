e89b4f7424f7174e1626eab34c51f0cc32dd0aa9
Juanjo Presa <Juanjo Presa>
2015-09-17 15:15:34 +0200

    Simplified vmwaretools profile

--- dist/profile/manifests/vmtools.pp
+++ dist/profile/manifests/vmtools.pp
@@ -1,33 +1,10 @@
 class profile::vmtools {
 
-  $vmtools_mode = $::operatingsystem ? {
-    'Ubuntu' => $::lsbdistrelease ? {
-      '10.04' => $::rol ? {
-        'siv'   => 'vmwaretools', #workaround because no vmwaretools for 10.04 siv $::kernelrelease (2.6.32-40-generic) version.
-        default => 'vmwaretools',
-      },
-      default => 'openvmtools',
-    },
-    'CentOS' => $::lsbdistrelease ? {
-      /5.*/     => 'vmwaretools',
-      /6.*/     => 'vmwaretools',
-      default => 'openvmtools',
-    },
-  default => 'openvmtools',
+  class { 'vmwaretools':
+    version         => '9.0.10-1481436',
+    working_dir     => '/tmp/vmwaretools',
+    archive_url     => 'http://16.0.96.20/repo/vmwaretools',
+    archive_md5     => '0a11d80baf58a1bd4df2e2a8c5b32a24',
+    manage_dev_pkgs => false,
   }
-
-
-  if $vmtools_mode == 'vmwaretools' {
-    class { 'vmwaretools':
-      version         => '9.0.10-1481436',
-      working_dir     => '/tmp/vmwaretools',
-      archive_url     => 'http://16.0.96.20/repo/vmwaretools',
-      archive_md5     => '0a11d80baf58a1bd4df2e2a8c5b32a24',
-      manage_dev_pkgs => false,
-    }
-  }
-  if $vmtools_mode == 'openvmtools' {
-    include ::openvmtools
-  }
-
 }
