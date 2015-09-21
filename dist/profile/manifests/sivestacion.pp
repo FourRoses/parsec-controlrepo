092f5388a6bfcef5c32ad06c56b2f5945410bed5
Juanjo Presa <Juanjo Presa>
2015-09-21 10:37:46 +0200

    Changed 71 net rules for no be overwritted

--- dist/profile/manifests/sivestacion.pp
+++ dist/profile/manifests/sivestacion.pp
@@ -19,7 +19,7 @@ class profile::sivestacion {
     netmask   => $::foreman_interfaces[0][attrs][netmask],
     gateway   => regsubst($::foreman_interfaces[0][ip], '^(\d+\.\d+\.\d+)\.\d+$', '\1.1')
   }
-  udev::rule { '70-persistent-net.rules':
+  udev::rule { '71-persistent-net.rules':
     ensure  => present,
     content => "SUBSYSTEM==\"net\", ACTION==\"add\", DRIVERS==\"?*\", ATTR{address}==\"${::foreman_interfaces[0][mac]}\", ATTR{dev_id}==\"0x0\", ATTR{type}==\"1\", KERNEL==\"eth*\", NAME=\"${::foreman_interfaces[0][identifier]}\"\n",
   }
