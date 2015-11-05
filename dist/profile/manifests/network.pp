d0c140c11229564c2114ee5f45a132a8cde7c233
Juanjo Presa <Juanjo Presa>
2015-11-05 15:14:28 +0100

    Fixed typo

--- dist/profile/manifests/network.pp
+++ dist/profile/manifests/network.pp
@@ -6,7 +6,7 @@ class profile::network {
     gateway   => regsubst($::foreman_interfaces[0][ip], '^(\d+\.\d+\.\d+)\.\d+$', '\1.1')
   }
 
-  if $::osfamiliy == 'Debian' {
+  if $::osfamily == 'Debian' {
     udev::rule { '71-persistent-net.rules':
       ensure  => present,
       content => "SUBSYSTEM==\"net\", ACTION==\"add\", DRIVERS==\"?*\", ATTR{address}==\"${::foreman_interfaces[0][mac]}\", ATTR{dev_id}==\"0x0\", ATTR{type}==\"1\", KERNEL==\"eth*\", NAME=\"${::foreman_interfaces[0][identifier]}\"\n",
