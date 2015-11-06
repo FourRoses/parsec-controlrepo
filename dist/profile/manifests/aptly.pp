d505498438d5487fed35b99ec0aeff826d0727d9
Juanjo Presa <Juanjo Presa>
2015-11-06 14:42:08 +0100

    Added atchs to mirror

--- dist/profile/manifests/aptly.pp
+++ dist/profile/manifests/aptly.pp
@@ -6,11 +6,11 @@ class profile::aptly {
   }
 
   apt::source { 'aptly':
-    ensure      => present,
-    location    => 'http://16.0.96.20:3142/repo.aptly.info',
-    release     => 'squeeze',
-    repos       => 'main',
-    key         => {
+    ensure   => present,
+    location => 'http://16.0.96.20:3142/repo.aptly.info',
+    release  => 'squeeze',
+    repos    => 'main',
+    key      => {
       'id'      => 'B6140515643C2AE155596690E083A3782A194991',
       'server'  => 'keys.gnupg.net',
       'options' => 'http-proxy="http://16.0.96.20:3128"',
@@ -25,8 +25,9 @@ class profile::aptly {
   }
 
   aptly::mirror { 'puppetlabs':
-    location => 'http://apt.puppetlabs.com/',
-    repos    => ['main', 'dependencies'],
-    key      => '4BD6EC30',
+    location      => 'http://apt.puppetlabs.com/',
+    repos         => ['main', 'dependencies'],
+    key           => '4BD6EC30',
+    architectures => ['i386', 'amd64'],
   }
 }
